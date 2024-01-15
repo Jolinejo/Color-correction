1b5b0ab1d1ee253c6940afe7b6414491e453a409
Mariam Ibrahim <117775223+Jolinejo@users.noreply.github.com>
2024-01-15 15:37:31 +0200

    app working

--- scripts/script.js
+++ scripts/script.js
@@ -6,10 +6,31 @@ inputFile.addEventListener("change", uploadImage);
 
 
 function uploadImage(){
-    let imgLink = URL.createObjectURL(inputFile.files[0]);
-    imgView.style.backgroundImage = `url(${imgLink})`;
-    imgView.textContent = "";
-    imgView.style.backgroundSize = 'contain'; // or 'cover'
-    imgView.style.backgroundRepeat = 'no-repeat';
-    imgView.style.backgroundPosition = 'center center';
+    const file = inputFile.files[0];
+    if (!file) {
+        return; // Handle the case where no file is selected
+    }
+
+    const formData = new FormData();
+    formData.append("image", file);
+
+    fetch('http://127.0.0.1:5001/upload', {
+        method: 'POST',
+        body: formData
+    })
+    .then(response => response.json())
+    .then(data => {
+      if (data.error) {
+        console.error("Error processing image:", data.error);
+        // Display an error message to the user
+      } else {
+        const img = document.createElement('img');
+        img.src = 'data:image/jpeg;base64,' + data.image;
+        imgView.innerHTML = ''; // Clear any previous content
+        imgView.style.backgroundImage = `url(${img.src})`;
+        imgView.style.backgroundSize = 'contain'; // or 'cover'
+        imgView.style.backgroundRepeat = 'no-repeat';
+        imgView.style.backgroundPosition = 'center center';
+      }
+    })
 }
